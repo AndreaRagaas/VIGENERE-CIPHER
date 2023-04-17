21ca2af5c592b136c6a47e58bae4e83a40b8e81c
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-17 13:31:06 +0800

    DEFINING THE FUNCTIONS NEEDED

--- VIGENERE CIPHER.py
+++ VIGENERE CIPHER.py
@@ -2,3 +2,20 @@ import pyfiglet as pyg
 from termcolor import colored
 
 print("NOTE: TYPE THE MESSAGE AND KEYWORK IN ALL UPPERCASE LETTERS WITH NO SPACES")
+
+def generateKey(string, key): 
+    key = list(key) 
+    if len(string) == len(key): 
+        return(key) 
+    else: 
+        for i in range(len(string) -len(key)): 
+            key.append(key[i % len(key)]) 
+    return("" . join(key))
+  
+def cipher(string, key): 
+    cipher_text = [] 
+    for i in range(len(string)): 
+       x = (ord(string[i]) +ord(key[i])) % 26
+       x += ord('A') 
+       cipher_text.append(chr(x)) 
+    return("" . join(cipher_text)) 
